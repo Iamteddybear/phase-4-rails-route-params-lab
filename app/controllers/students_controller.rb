6e205269c8b092c03846921add9b53fa048868df
ned <nedschultz@outlook.com>
2023-06-21 12:57:37 +1000

    git clone

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,13 @@
 class StudentsController < ApplicationController
-
   def index
     students = Student.all
     render json: students
   end
 
+  def show
+    student = Student.find(params[:id])
+    render json: student
+  rescue ActiveRecord::RecordNotFound
+    render json: { error: "Student not found" }, status: :not_found
+  end
 end
